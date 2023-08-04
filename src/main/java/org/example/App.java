b75470e1d23eb08e65032eb7a1afe3396f961872
Vitali Sinitski <vitali.sin777@gmail.com>
2023-08-04 23:06:30 +0300

    First commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String text = "Hello new day!";
+        printSomeText(text);
+    }
+
+    private static void printSomeText(String text) {
+        System.out.println(text);
+    }
+}
